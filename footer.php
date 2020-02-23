a3f1ad3efc6d91d1ba9b2b242d504e04845baa93
Talanda Williams <talandaw@gmail.com>
2020-02-22 23:34:18 -0800

    Update index file to use header/footer/script import files

---
+++ footer.php
@@ -0,0 +1,52 @@
+<!-- Start Footer -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <!-- Start Footer Widgets -->
+            <div class="col-md-6 col-sm-6 widget footer-widget">
+                <h4 class="footer-widget-title">Naginata</h4>
+                <p>A Japanese martial art of both power and grace, is characterized by the grandeur of its sweeping
+                    movements. Naginata is for people of all ages interested in either competitive fighting, or in the
+                    physical
+                    beauty of choreographed, practiced movements, called kata.</p>
+            </div>
+            <div class="col-md-6 col-sm-6 widget footer-widget">
+                <h4 class="footer-widget-title">Quick Links</h4>
+                <div class="col-md-6">
+                    <ul>
+                        <li><a href="index">Home</a></li>
+                        <li><a href="about">About the USNF</a></li>
+                        <li><a href="federations">Regional Federations</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <ul>
+                        <li><a href="calendar">Calendar of Events</a></li>
+                        <li><a href="#" data-placement="left" data-toggle="tooltip" data-original-title="Coming Soon!">Members
+                                Dashboard</a></li>
+                        <li><a href="contact">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="site-footer-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="copyrights-col-left col-md-6 col-sm-6">
+                <p>&copy; 2020 USNF. All Rights Reserved</p>
+            </div>
+            <div class="copyrights-col-right col-md-6 col-sm-6">
+                <div class="social-icons">
+                    Connect with USNF & the Naginata Community:
+                    <a href="https://www.facebook.com/United-States-Naginata-Federation-925385317560651" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="https://twitter.com/hashtag/naginata?lang=en" target="_blank"><i class="fa fa-twitter"></i></a>
+                    <a href="https://www.youtube.com/results?search_query=naginata+usnf" target="_blank"><i class="fa fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+<a id="back-to-top"><i class="fa fa-angle-double-up"></i></a> </div>
