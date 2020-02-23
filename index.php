71a4299112617a5d82683aee47a59a05ce971c7a
Talanda Williams <talandaw@gmail.com>
2020-02-23 14:45:07 -0800

    index page changes

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <!-- Start Hero Slider -->
 <div class="hero-slider flexslider clearfix" data-autoplay="yes" data-pagination="yes" data-arrows="yes" data-style="fade" data-pause="yes">
     <ul class="slides tinted-image">
-        <li class="parallax" style="background-image:url(images/banners/USNFBanner1.jpg);"></li>
-        <li class="parallax" style="background-image:url(images/banners/USNFBanner3.jpg);"></li>
-        <li class="parallax" style="background-image:url(images/banners/USNFBanner6.jpg);"></li>
-        <li class="parallax" style="background-image:url(images/banners/USNFBanner9.jpg);"></li>
+        <li class="parallax" style="background-image:url(./images/banners/USNFBanner1.jpg);"></li>
+        <li class="parallax" style="background-image:url(./images/banners/USNFBanner3.jpg);"></li>
+        <li class="parallax" style="background-image:url(./images/banners/USNFBanner6.jpg);"></li>
+        <li class="parallax" style="background-image:url(./images/banners/USNFBanner9.jpg);"></li>
     </ul>
 </div>
 <!-- End Hero Slider -->
@@ -18,25 +18,25 @@
             <div class="row">
                 <!-- Start Featured Blocks -->
                 <div class="featured-blocks clearfix">
-                    <div class="col-md-4 col-sm-4 featured-block"><a href="about.html" class="img-thumbnail">
-                            <img src="images/banners/USNFBanner4.jpg" alt="What's the USNF?"> <strong>What's
+                    <div class="col-md-4 col-sm-4 featured-block"><a href="./about" class="img-thumbnail">
+                            <img src="./images/banners/USNFBanner4.jpg" alt="What's the USNF?"> <strong>What's
                                 the USNF?</strong> <span class="more">learn more</span> </a></div>
-                    <div class="col-md-4 col-sm-4 featured-block"><a href="federations.html" class="img-thumbnail">
-                            <img src="images/banners/USNFBanner7.jpg" alt="Where Can I Practice?"> <strong>Where
+                    <div class="col-md-4 col-sm-4 featured-block"><a href="./federations" class="img-thumbnail">
+                            <img src="./images/banners/USNFBanner7.jpg" alt="Where Can I Practice?"> <strong>Where
                                 Can I Practice?</strong> <span class="more">find a federation</span> </a></div>
-                    <div class="col-md-4 col-sm-4 featured-block"><a href="membership" class="img-thumbnail">
-                            <img src="images/banners/USNFBanner8.jpg" alt="How can I join"> <strong>How can I
-                                join?</strong> <span class="more">apply now</span> </a></div>
+                    <div class="col-md-4 col-sm-4 featured-block"><a href="./contact" class="img-thumbnail">
+                            <img src="./images/banners/USNFBanner8.jpg" alt="How can I join"> <strong>How can I
+                                join?</strong> <span class="more">contact us</span> </a></div>
                 </div>
                 <!-- End Featured Blocks -->
             </div>
-        <div class="row">
-          <div class="col-md-8 col-sm-6"> 
-            <!-- Events Listing -->
-            <div class="listing events-listing">
-              <header class="listing-header">
-                <h3>Upcoming Events
-                    <a href="calendar.html" class="pull-right btn btn-info">View Calendar</a>
+            <div class="row">
+                <div class="col-md-8 col-sm-6">
+                    <!-- Events Listing -->
+                    <div class="listing events-listing">
+                        <header class="listing-header">
+                            <h3>Upcoming Events
+                                <a href="calendar.html" class="pull-right btn btn-info">View Calendar</a>
                 </h3>
               </header>
               <section class="listing-cont">
@@ -60,54 +60,75 @@
                     <div class="event-date"><span class="date">7</span> <span class="month">Dec</span></div>
                     <div class="event-detail">
                         <h4>PNNF: 4th Annual Taikai</h4>
-                      <span class="event-dayntime meta-data">Merer Island Community and Event Center</span></div>
+                        <span class="event-dayntime meta-data">Merer Island Community and Event Center</span></div>
                   </li>
                 </ul>
               </section>
-            </div>
-            <div class="spacer-30"></div>
-            <!-- Latest News -->
-            <div class="listing post-listing">
-              <header class="listing-header">
-                <h3>Latest News</h3>
-              </header>
-              <section class="listing-cont">
-                <ul>
-                  <li class="item post">
-                    <div class="row">
-                      <div class="col-md-4"><a href="#" class="media-box">
-                        <img src="images/save-the-date.jpg" alt="" class="img-thumbnail"> </a></div>
-                      <div class="col-md-8">
-                        <div class="post-title">
-                            <h2><a href="blog-post">Save the Date!</a></h2>
-                          <span class="meta-data"><i class="fa fa-calendar"></i> July 29th - August 2nd, 2020</span>
-                        </div>
-                        <p>We have a date and location selected for the USNF Seminar, Taikai, and Shinsa.
-                          More information will be announced when available.
-                        </p>
-                      </div>
                     </div>
-                  </li>
-                </ul>
-              </section>
-            </div>
-          </div>
-          <!-- Start Sidebar -->
-          <div class="col-md-4 col-sm-6"> 
-            <!-- Latest Sermons -->
-            <div class="listing sermons-listing">
-              <header class="listing-header">
-                <h3>Media</h3>
-              </header>
-              <section class="listing-cont">
-                <ul>
-                  <li class="item sermon featured-sermon"><span class="date">Feb 14, 2019</span>
-                    <h4>Video Spotlight: </h4>
-                    <div class="featured-sermon-video">
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/CgswGo8X8nc" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div class="spacer-30"></div>
+                    <!-- Latest News -->
+                    <div class="listing post-listing">
+                        <header class="listing-header">
+                            <h3>Important Updates</h3>
+                        </header>
+                        <section class="listing-cont">
+                            <ul>
+                                <li class="item post">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <img src="./images/vote-election.jpg" alt="" class="img-thumbnail">
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <div class="post-title">
+                                                <h2><a href="blog-post">USNF 2020 Annual General Meeting and
+                                                        Election</a></h2>
+                                                <span class="meta-data"><i class="fa fa-calendar"></i> March 14, 2020</span>
+                                            </div>
+                                            <p>Absentee ballots and instructions were sent out and must be
+                                                <em>received</em> by the USNF Executive Secretary by March 14.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li class="item post">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <img src="./images/save-the-date.jpg" alt="" class="img-thumbnail">
+                                        </div>
+                                        <div class="col-sm-9">
+                                            <div class="post-title">
+                                                <h2><a href="blog-post">USNF 2020 Seminar, Taikai, and Shinsa</a></h2>
+                                                <span class="meta-data"><i class="fa fa-calendar"></i> July 29th - August 2nd, 2020</span>
+                                            </div>
+                                            <p>We have a date and location selected for the USNF Seminar, Taikai, and
+                                                Shinsa.
+                                                More information will be announced when available.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </section>
                     </div>
-                    <p>WNC - ENGI - B16 (3rd place) Canada vs USA</p>
-                      <p>Part of the Naginata Israel playlist
+                </div>
+                <!-- Start Sidebar -->
+                <div class="col-md-4 col-sm-6">
+                    <!-- Latest Sermons -->
+                    <div class="listing sermons-listing">
+                        <header class="listing-header">
+                            <h3>External Media</h3>
+                        </header>
+                        <section class="listing-cont">
+                            <ul>
+                                <li class="item sermon featured-sermon"><span class="date">Feb 14, 2019</span>
+                                    <h4>Video Spotlight: </h4>
+                                    <div class="featured-sermon-video">
+                                        <iframe width="560" height="315" src="https://www.youtube.com/embed/CgswGo8X8nc" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    </div>
+                                    <p>WNC - ENGI - B16 (3rd place) Canada vs USA</p>
+                                    <p>Part of the Naginata Israel playlist
                           <a href="https://www.youtube.com/playlist?list=PLd5sEtT07GCBGke5LYqlP-uY_h0MaQWPq">found
                               here.</a>
                           Thank you!
