1c55e72696fa31afade1ab71c06fd7aab3332f7b
Talanda Williams <talandaw@gmail.com>
2020-02-23 17:54:35 -0800

    champions partial tables

--- champions.php
+++ champions.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <ol class="breadcrumb">
                     <li><a href="./">Home</a></li>
-                    <li><a href="./about">About the USNF</a></li>
+                    <li><a href="./about">About Naginata & the USNF</a></li>
                     <li class="active">US Champions</li>
                 </ol>
             </div>
@@ -19,224 +19,131 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1>Typography</h1>
+                <h1>US Champions</h1>
             </div>
         </div>
     </div>
-  </div>
-  <!-- End Page Header --> 
-  <!-- Start Content -->
-  <div class="main" role="main">
+</div>
+<!-- End Page Header -->
+<!-- Start Content -->
+<div class="main" role="main">
     <div id="content" class="content full">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <h1>H1 heading style</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h2>H2 heading style</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h3>H3 heading style</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h4>H4 heading style</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h5>H5 heading style</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h6>H6 heading style</h6>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Suspendisse consectetur fringilla luctus. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-          </div>
-          <div class="col-md-6">
-            <h2>Blockquote</h2>
-            <blockquote>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-              <cite>- Author name here</cite> </blockquote>
-            <h2>Labels</h2>
-            <p><span class="label label-primary">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <p><span class="label label-danger">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <p><span class="label label-info">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <p><span class="label label-success">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <p><span class="label label-warning">Lorem</span> ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-            <h2>Paragraph</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-          </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="text-primary">18th USNF Championship (2018) - Bryn Mawr College, PA</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Match</th>
+                            <th>Place</th>
+                            <th>Federation</th>
+                            <th colspan="3" scope="colgroup" class="text-align-center">Name(s)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td rowspan="3" scope="rowgroup" class="text-align-center" style="vertical-align: middle">
+                                Men's Team
+                            </td>
+                            <td scope="row">1st Place</td>
+                            <td>NCNF</td>
+                            <td>Saiyou Ohshima</td>
+                            <td>Xinyuan Lai</td>
+                            <td>James Libby</td>
+                        </tr>
+                        <tr>
+                            <td scope="row">2nd Place</td>
+                            <td>ECNF</td>
+                            <td>Axel Noorman</td>
+                            <td>Ellery Engalla</td>
+                        </tr>
+                        <tr>
+                            <td scope="row">3rd Place</td>
+                            <td>SCNF</td>
+                            <td>Dan Trexler</td>
+                            <td>Juan Hernandez</td>
+                            <td>Richard Metzger</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Match</th>
+                            <th>Place</th>
+                            <th>Federation</th>
+                            <th colspan="3" scope="colgroup" class="text-align-center">Name(s)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td rowspan="3" scope="rowgroup" class="text-align-center" style="vertical-align: middle">
+                                Women's Team
+                            </td>
+                            <td scope="row">1st Place</td>
+                            <td>GNYNF</td>
+                            <td>Sasha Corchado</td>
+                            <td>Katie Roche</td>
+                            <td>Mary Quien</td>
+                        </tr>
+                        <tr>
+                            <td scope="row">2nd Place</td>
+                            <td>SCNF</td>
+                            <td>Grace Wu</td>
+                            <td>Shannon Lew</td>
+                            <td>Jennifer Bernot</td>
+                        
+                        </tr>
+                        <tr>
+                            <td scope="row">3rd Place</td>
+                            <td>NCNF</td>
+                            <td>Veronica Gunawan</td>
+                            <td>Talanda William</td>
+                            <td>Yuki Nishimura</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>Match</th>
+                            <th>Place</th>
+                            <th>Federation</th>
+                            <th>Name(s)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td rowspan="3" scope="rowgroup" class="text-align-center" style="vertical-align: middle">
+                                Men's Individuals (Yudansha)
+                            </td>
+                            <td scope="row">1st Place</td>
+                            <td>ECNF</td>
+                            <td>Axel Noorma</td>
+                        </tr>
+                        <tr>
+                            <td scope="row">2nd Place</td>
+                            <td>GNYNF</td>
+                            <td>C.L. Chen</td>
+                        </tr>
+                        <tr>
+                            <td scope="row">3rd Place</td>
+                            <td>GNYNF</td>
+                            <td>Patrick Magpayo</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                
+                </div>
+            </div>
         </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Dropcaps</h2>
-          </div>
-          <div class="col-md-6">
-            <p class="drop-caps">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-          </div>
-          <div class="col-md-6">
-            <p class="drop-caps secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor.</p>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Lists</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <h4>Unordered List</h4>
-            <ul class="unordered">
-              <li>Lorem ipsum dolor sit amet</li>
-              <li>Consectetur adipiscing elit</li>
-              <li>Integer molestie lorem at massa</li>
-              <li>Facilisis in pretium nisl aliquet</li>
-              <li> Nulla volutpat aliquam velit
-                <ul>
-                  <li>Phasellus iaculis neque</li>
-                  <li>Purus sodales ultricies</li>
-                  <li>Vestibulum laoreet porttitor sem</li>
-                  <li>Ac tristique libero volutpat at</li>
-                </ul>
-              </li>
-              <li>Faucibus porta lacus fringilla vel</li>
-              <li>Aenean sit amet erat nunc</li>
-              <li>Eget porttitor lorem</li>
-            </ul>
-            <h4 class="spaced">Description List</h4>
-            <dl>
-              <dt>Description lists</dt>
-              <dd>A description list is perfect for defining terms.</dd>
-              <dt>Euismod</dt>
-              <dd>Vestibulum id ligula porta felis euismod semper eget lacinia odio sem nec elit.</dd>
-              <dd>Donec id elit non mi porta gravida at eget metus.</dd>
-              <dt>Malesuada porta</dt>
-              <dd>Etiam porta sem malesuada magna mollis euismod.</dd>
-            </dl>
-          </div>
-          <div class="col-md-6">
-            <h4>Ordered List</h4>
-            <ol>
-              <li>Facilisis in pretium nisl aliquet</li>
-              <li>Faucibus porta lacus fringilla vel</li>
-              <li>Aenean sit amet erat nunc</li>
-              <li>Eget porttitor lorem</li>
-              <li>Consectetur adipiscing elit</li>
-              <li>Integer molestie lorem at massa</li>
-              <li>Facilisis in pretium nisl aliquet</li>
-              <li>Faucibus porta lacus fringilla vel</li>
-              <li>Aenean sit amet erat nunc</li>
-            </ol>
-            <h4 class="spaced">Icons List</h4>
-            <ul class="hearts">
-              <li><i class="icon icon-envelope-alt"></i> Fusce sit amet orci quis arcu vestibulum vestibulum sed ut felis. </li>
-              <li><i class="icon icon-film"></i> Phasellus in risus quis lectus iaculis vulputate id quis nisl.</li>
-            </ul>
-            <ul class="checks">
-              <li><i class="icon icon-envelope-alt"></i> Fusce sit amet orci quis arcu vestibulum vestibulum sed ut felis. </li>
-              <li><i class="icon icon-film"></i> Phasellus in risus quis lectus iaculis vulputate id quis nisl.</li>
-            </ul>
-            <ul class="carets">
-              <li><i class="icon icon-envelope-alt"></i> Fusce sit amet orci quis arcu vestibulum vestibulum sed ut felis. </li>
-              <li><i class="icon icon-film"></i> Phasellus in risus quis lectus iaculis vulputate id quis nisl.</li>
-            </ul>
-            <ul class="chevrons">
-              <li><i class="icon icon-envelope-alt"></i> Fusce sit amet orci quis arcu vestibulum vestibulum sed ut felis. </li>
-              <li><i class="icon icon-film"></i> Phasellus in risus quis lectus iaculis vulputate id quis nisl.</li>
-            </ul>
-            <h4 class="spaced">Inline</h4>
-            <ul class="inline">
-              <li>Lorem ipsum</li>
-              <li>Phasellus iaculis</li>
-              <li>Nulla volutpat</li>
-            </ul>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Code</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <h4>Inline</h4>
-            <p>For example, <code>&lt;section&gt;</code> should be wrapped as inline.</p>
-          </div>
-          <div class="col-md-6">
-            <h4>Basic Block</h4>
-            <pre>&lt;p&gt;Sample text here...&lt;/p&gt;</pre>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="col-md-12">
-            <h2>Tables</h2>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <h4>Striped</h4>
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th> # </th>
-                  <th> First Name </th>
-                  <th> Last Name </th>
-                  <th> Username </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td> 1 </td>
-                  <td> Mark </td>
-                  <td> Otto </td>
-                  <td> @mdo </td>
-                </tr>
-                <tr>
-                  <td> 2 </td>
-                  <td> Jacob </td>
-                  <td> Thornton </td>
-                  <td> @fat </td>
-                </tr>
-                <tr>
-                  <td> 3 </td>
-                  <td> Larry </td>
-                  <td> the Bird </td>
-                  <td> @twitter </td>
-                </tr>
-              </tbody>
-            </table>
-            <h4 class="spaced">Bordered</h4>
-            <table class="table table-bordered">
-              <thead>
-                <tr>
-                  <th> # </th>
-                  <th> First Name </th>
-                  <th> Last Name </th>
-                  <th> Username </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td> 1 </td>
-                  <td> Mark </td>
-                  <td> Otto </td>
-                  <td> @mdo </td>
-                </tr>
-                <tr>
-                  <td> 2 </td>
-                  <td> Jacob </td>
-                  <td> Thornton </td>
-                  <td> @fat </td>
-                </tr>
-                <tr>
-                  <td> 3 </td>
-                  <td> Larry </td>
-                    <td> the Bird</td>
-                    <td> @twitter</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 <?php include('./footer.php'); ?>
 <?php include('./scripts.php'); ?>
 
