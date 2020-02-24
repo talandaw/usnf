2fbc2193d5723bb74485dc3e4df58c8ffd90505b
Talanda Williams <talandaw@gmail.com>
2020-02-23 16:39:40 -0800

    favicon update

--- header.php
+++ header.php
@@ -17,6 +17,11 @@
     <meta name="format-detection" content="telephone=no">
     <!-- CSS
       ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    
     <link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="./plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
     <link href="./css/style.css" rel="stylesheet" type="text/css">
