594cdee37ca781f9a8c905d32dd0750aff5b721c
Talanda Williams <talandaw@gmail.com>
2020-03-01 16:02:55 -0800

    upgrade full calendar to 2.9.1 from 2.2

--- calendar.php
+++ calendar.php
@@ -1,15 +1,191 @@
-<?php include('./header.php'); ?>
+<!DOCTYPE HTML>
+<head>
+    <!-- Basic Page Needs
+      ================================================== -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>USNF | United States Naginata Federation Website</title>
+    <meta name="description" content="United States Naginata Federation website for providing information about USNF activites.
+            A Japanese martial art of both power and grace, is characterized by the grandeur of its sweeping
+            movements. Naginata is for people of all ages interested in either competitive fighting, or in the physical
+            beauty of choreographed, practiced movements, called kata.">
+    <meta name="keywords" content="USNF, US Naginata, Naginata Federation, ECNF, NCNF, SCNF, GNYNF, RMNF, PCNF,
+                        ">
+    <meta name="author" content="Talanda">
+    <!-- Mobile Specific Metas
+      ================================================== -->
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <!-- CSS
+      ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
 
-<!-- Start Nav Backed Header -->
-<div class="nav-backed-header parallax">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <ol class="breadcrumb">
-                    <li><a href="./">Home</a></li>
-                    <li class="active">Calendar</li>
-                </ol>
+    <link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="./plugins/mediaelement/mediaelementplayer.css" rel="stylesheet" type="text/css">
+    <link href="./css/style.css?ver=1.0" rel="stylesheet" type="text/css">
+    <link href="./plugins/prettyphoto/css/prettyPhoto.css" rel="stylesheet" type="text/css">
+    <link href='plugins/fullcalendar/fullcalendar.css' rel='stylesheet'/>
+    <link href='plugins/fullcalendar/fullcalendar.print.css' rel='stylesheet' media='print'/>
+    <!--[if lte IE 8]>
+    <link rel="stylesheet" type="text/css" href="./css/ie8.css" media="screen"/><![endif]-->
+    <link href="./css/custom.css?ver=1.0" rel="stylesheet" type="text/css">
+    <!-- SCRIPTS
+      ================================================== -->
+    <script src="js/modernizr.js"></script><!-- Modernizr -->
+</head>
+<body>
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser
+    today</a> to better experience this site.</p>
+<![endif]-->
+<div class="body">
+    <!-- Start Site Header -->
+    <header class="site-header">
+        <div class="topbar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-8">
+                        <h1 class="logo"><a href="./">
+                                <img src="./images/usnf-mon.png" alt="USNF Logo" class="img-responsive" width="70px"></a>
+                            | United States Naginata Federation
+                        </h1>
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-4">
+                        <ul class="top-navigation hidden-sm hidden-xs">
+                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <input type="hidden" name="cmd" value="_s-xclick"/>
+                                <input type="hidden" name="hosted_button_id" value="BR3V8P2P22TWE"/>
+                                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button"/>
+                                <img alt="" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+                            </form>
+                        </ul>
+                        <a href="#" class="visible-sm visible-xs menu-toggle"><i class="fas fa-bars"></i></a></div>
+                </div>
             </div>
+        </div>
+        <div class="main-menu-wrapper">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <nav class="navigation">
+                            <ul class="sf-menu">
+                                <li><a href="./">Home</a></li>
+                                <li><a href="#">About the USNF</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./about">What is Naginata?</a></li>
+                                        <li><a href="./about#board">Board of Directors</a></li>
+                                        <li><a href="./champions">US Champions</a></li>
+                                        <li><a href="./history">USNF History</a></li>
+                                    </ul>
+                                </li>
+                                <li class="megamenu"><a href="#">Regional Federations</a>
+                                    <ul class="dropdown">
+                                        <li>
+                                            <div class="megamenu-container container">
+                                                <div class="row">
+                                                    <div class="col-md-4 hidden-sm hidden-xs">
+                                                        <span class="megamenu-sub-title"><i class="fa fa-photo"></i> Featured Media</span>
+                                                        <!--<iframe width="200" height="150" src="http://player.vimeo.com/video/19564018?title=0&amp;byline=0&amp;color=007F7B"></iframe>-->
+                                                        <img src="./images/usnf-participants.JPG" height="150" alt="USA Participants, 2019 Germany INF">
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <span class="megamenu-sub-title"><i class="fa fa-map-marker"></i> Our Locations</span>
+                                                        <ul class="sub-menu">
+                                                            <li><a href="http://www.ecnf.net/" target="_blank">East
+                                                                    Coast</a></li>
+                                                            <li><a href="http://www.gnynf.org/" target="_blank">Greater
+                                                                    New York</a></li>
+                                                            <li><a href="https://ncnf.tidyhq.com/" target="_blank">Northern
+                                                                    California</a></li>
+                                                            <li><a href="http://www.pnnf.org/" target="_blank">Pacific
+                                                                    Northwest</a></li>
+                                                            <li>
+                                                                <a href="#" data-placement="left" data-toggle="tooltip" data-original-title="Sorry, No Website">Rocky
+                                                                    Mountain</a></li>
+                                                            <li><a href="https://www.scnf.org/" target="_blank"">Southern
+                                                                California</a></li>
+                                                            <span class="meta-data">Please note: These link to each Federation's external site and open in new tabs or windows.</span>
+                                                        </ul>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <span class="megamenu-sub-title"><i class="fa fa-globe"></i> Discover More</span>
+                                                        <ul class="sub-menu">
+                                                            <li><a href="./federations">About Our Regional
+                                                                    Federations</a><span class="meta-data">View the Map</span>
+                                                            </li>
+                                                            <li><a href="./federations#international">View
+                                                                    International Federation
+                                                                    Info</a><span class="meta-data">Not in the United States?</span>
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="#">Gallery</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./us-gallery.php">USNF Events</a></li>
+                                        <li><a href="#">Regional Galleries</a>
+                                            <ul class="dropdown">
+                                                <li><a href="ecnf-gallery">East Coast</a></li>
+                                                <li><a href="gnynf-gallery">Greater New York</a></li>
+                                                <li><a href="ncnf-gallery">Northern California</a></li>
+                                                <li><a href="pcnf-gallery">Pacific Northwest</a></li>
+                                                <li><a href="rmnf-gallery">Rocky Mountain</a></li>
+                                                <li><a href="scnf-gallery">Southern California</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="#">For Members</a>
+                                    <ul class="dropdown">
+                                        <li>
+                                            <a href="#" class="btn btn-primary" data-placement="left" data-toggle="tooltip" data-original-title="Coming Soon!">Member
+                                                Login</a></li>
+                                        <li><a href="membership">Membership Information</a></li>
+                                        <li><a href="testing-paperwork">Dan & Kyu Testing Paperwork</a></li>
+                                        <li><a href="ranking-referee">Ranking & Referee Regulations</a></li>
+                                        <li><a href="shogo-application">Shogo Application</a></li>
+                                        <li><a href="examiners-forms">Examiner's Forms</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Ranking</a></li>
+                                                <li><a href="#">Referee</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="tournament-forms">Tournament Forms</a>
+                                            <ul class="dropdown">
+                                                <li><a href="#">Ranking</a></li>
+                                                <li><a href="#">Referee</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="expense-report">Expense Report</a></li>
+                                        <li><a href="bylaws">By-laws</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- End Site Header -->
+    <!-- Start Nav Backed Header -->
+    <div class="nav-backed-header parallax">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ol class="breadcrumb">
+                        <li><a href="./">Home</a></li>
+                        <li class="active">Calendar</li>
+                    </ol>
+                </div>
         </div>
     </div>
 </div>
@@ -24,24 +200,24 @@
         </div>
     </div>
 </div>
-<!-- End Page Header -->
-<!-- Start Content -->
-<div class="main" role="main">
-    <div id="content" class="content full">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <h1>Pardon our dust, an updated calendar will be uploaded & finalized soon!</h1>
-                    <img src="./images/calendar.jpg" class="img-responsive center-block">
-                    <!--
-                    <div id='loading'>Loading Calendar...</div>
-                    <div id='calendar'></div>
-                    -->
+    <!-- End Page Header -->
+    <!-- Start Content -->
+    <div class="main" role="main">
+        <div id="content" class="content full">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div id='loading'>
+                            <h1>Loading Calendar...</h1>
+                            <img src="./images/calendar.jpg" class="img-responsive center-block">
+                        </div>
+                        <div id='calendar'></div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 <?php include('./footer.php'); ?>
 
 <script src='plugins/fullcalendar/lib/moment.min.js'></script>
@@ -52,33 +228,49 @@
 <script src="js/waypoints.js"></script> <!-- Waypoints -->
 <script src="plugins/mediaelement/mediaelement-and-player.min.js"></script> <!-- MediaElements -->
 <script src="js/init.js"></script> <!-- All Scripts -->
-<script src="plugins/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
-<script src="plugins/countdown/js/jquery.countdown.min.js"></script> <!-- Jquery Timer -->
-<script src='plugins/fullcalendar/jquery-ui.custom.min.js'></script>
-<script src='plugins/fullcalendar/fullcalendar.min.js'></script>
-<script src='plugins/fullcalendar/gcal.js'></script> <!-- Google Calendar -->
-<script>
+    <script src="plugins/flexslider/js/jquery.flexslider.js"></script> <!-- FlexSlider -->
+    <script src="plugins/countdown/js/jquery.countdown.min.js"></script> <!-- Jquery Timer -->
+    <script src='plugins/fullcalendar/jquery-ui.custom.min.js'></script>
+    <script src='plugins/fullcalendar/fullcalendar.min.js'></script>
+    <script src='plugins/fullcalendar/gcal.js'></script> <!-- Google Calendar -->
+    <script>
 
-    $(document).ready(function () {
+        $(document).ready(function () {
 
-        $('#calendar').fullCalendar({
+            $('#calendar').fullCalendar({
+                //header: { center: 'month,agendaWeek,agendaDay' },
 
-            // THIS KEY WON'T WORK IN PRODUCTION!!!
-            // To make your own Google API key, follow the directions here:
-            // http://fullcalendar.io/docs/google_calendar/
-            googleCalendarApiKey: 'AIzaSyBIXBso5Hscakr2c_Vh6Glsw3n8iRqiJws',
+                // buttons for switching between views
+                header: {
+                    left: 'month,agendaWeek,listWeek',
+                    center: 'title',
+                    right: 'custom today prevYear,prev,next,nextYear'
+                },
 
-            // US Holidays
-            events: 'fm06kljukhiupdtmbqmidlfsgg@group.calendar.google.com',
+                customButtons: {
+                    custom: {
+                        text: 'custom',
+                        click: function () {
+                            alert('clicked custom button!');
+                        }
+                    }
+                },
+                // USNF Calendar API KEY
+                // To make your own Google API key, follow the directions here:
+                // http://fullcalendar.io/docs/google_calendar/
+                googleCalendarApiKey: 'AIzaSyAxDhc-w_kLGsqT2tFDbXUPvLgK_UTy36s',
 
-            eventClick: function (event) {
-                // opens events in a popup window
-                window.open(event.url, 'gcalevent', 'width=700,height=600');
-                return false;
-            },
+                // USNF Events Calendar
+                events: 'fm06kljukhiupdtmbqmidlfsgg@group.calendar.google.com',
 
-            loading: function (bool) {
-                $('#loading').toggle(bool);
+                eventClick: function (event) {
+                    // opens events in a popup window
+                    window.open(event.url, 'gcalevent', 'width=700,height=600');
+                    return false;
+                },
+
+                loading: function (bool) {
+                    $('#loading').toggle(bool);
             }
 
         });
