8902a13448c6bee2d583cc124c807ce74ed645b1
Talanda <talandaw@gmail.com>
2020-03-13 16:54:02 -0700

    membership page structure & file upload

---
+++ membership.php
@@ -0,0 +1,67 @@
+<?php include('./header.php'); ?>
+<!-- Start Nav Backed Header -->
+<div class="nav-backed-header parallax">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ol class="breadcrumb">
+                    <li><a href="./">Home</a></li>
+                    <li class="active">About Naginata & the USNF</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Nav Backed Header -->
+<!-- Start Page Header -->
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Membership Information</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Page Header -->
+<!-- Start Content -->
+<div class="main" role="main">
+    <div id="content" class="content full">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <h3 class="text-primary">Details</h3>
+                    <p>
+                        This form applies to new, current or members officially terminating their membership.
+                    <ol>
+                        <h4>Procedure:</h4>
+                        <li>For new members, please get approval from your chief instructor.</li>
+                        <li>Once approved by their chief instructor, new member then submits Membership Form to their
+                            regional executive secretary. Current members may also modify their membership or terminate
+                            membership accordingly. Please fill form completely. Payment to be sent to regional
+                            executive secretary who will then forward payment to USNF Office.
+                        </li>
+                        <li>Application forms must be submitted to regional executive secretary for recording and
+                            submission to USNF Executive Secretary.
+                        </li>
+                    </ol>
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <h3 class="text-primary">Form</h3>
+                    <a href="./files/membership-application.pdf" class="btn btn-success" download>Download Fillable Form
+                        <i class="fas fa-download"></i></a>
+                    <embed src="./files/membership-application.pdf" type="application/pdf"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include('./footer.php'); ?>
+<?php include('./scripts.php'); ?>
+
+
