f7b8b525f92e1345a1ef391fcc0d8e3f261bacd1
Talanda <talandaw@gmail.com>
2020-03-27 23:33:24 -0700

    galleries linked to google drive

--- gnynf-gallery.php
+++ gnynf-gallery.php
@@ -17,26 +17,8 @@
 <div class="page-header">
     <div class="container">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-xs-12">
                 <h1>GNYNF Gallery</h1>
-            </div>
-            <div class="col-md-6">
-                <div class="gallery-filter">
-                    <ul class="nav nav-pills sort-source" data-sort-id="gallery" data-option-key="filter">
-                        <li data-option-value="*" class="active"><a href="#"><i class="fa fa-th"></i>
-                                <span>Show All</span></a></li>
-                        <li data-option-value=".format-image"><a href="#"><i class="fa fa-picture-o"></i>
-                                <span>Image</span></a>
-                        </li>
-                        <li data-option-value=".format-link"><a href="#"><i class="fa fa-link"></i>
-                                <span>Link</span></a></li>
-                        <li data-option-value=".format-gallery"><a href="#"><i class="fa fa-camera"></i>
-                                <span>Slideshow</span></a>
-                        </li>
-                        <li data-option-value=".format-video"><a href="#"><i class="fa fa-play"></i>
-                                <span>Video</span></a></li>
-                    </ul>
-                </div>
             </div>
         </div>
     </div>
@@ -44,69 +26,11 @@
 <!-- End Page Header -->
 <!-- Start Content -->
 <div class="main" role="main">
-    <div id="content" class="content full">
-        <div class="container">
-            <div class="row">
-                <ul class="sort-destination" data-sort-id="gallery">
-                    <li class="col-md-4 col-sm-4 grid-item post format-image">
-                        <div class="grid-item-inner">
-                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-image">
-                        <div class="grid-item-inner">
-                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-gallery">
-                        <div class="grid-item-inner">
-                            <div class="media-box">
-                                <div class="flexslider" data-autoplay="yes" data-pagination="yes" data-arrows="yes" data-style="slide" data-pause="yes">
-                                    <ul class="slides">
-                                        <li class="item">
-                                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto[postname]"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""></a>
-                                        </li>
-                                        <li class="item">
-                                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto[postname]"><img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-link">
-                        <div class="grid-item-inner">
-                            <a href="http://www.google.com" target="_blank" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-image">
-                        <div class="grid-item-inner">
-                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-link">
-                        <div class="grid-item-inner">
-                            <a href="http://www.google.com" target="_blank" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-video">
-                        <div class="grid-item-inner">
-                            <a href="https://vimeo.com/19564018" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-video">
-                        <div class="grid-item-inner">
-                            <a href="http://youtu.be/NEFfnbQlGo8" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                    <li class="col-md-4 col-sm-4 grid-item post format-image">
-                        <div class="grid-item-inner">
-                            <a href="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" data-rel="prettyPhoto" class="media-box">
-                                <img src="http://placehold.it/600x400&amp;text=IMAGE+PLACEHOLDER" alt=""> </a></div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div id="content" class="container content">
+        <p>All of the images files are hosted on Google Drive, clicking any image will open a new link to the larger
+            version. </p>
+        <iframe src="https://drive.google.com/embeddedfolderview?id=1x8XTnKskQR6xDYgQN1F30ysMDD5dIavT#grid" width="100%"
+                height="600" frameborder="0"></iframe>
     </div>
 </div>
 <?php include('./footer.php'); ?>
